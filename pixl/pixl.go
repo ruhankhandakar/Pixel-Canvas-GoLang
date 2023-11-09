40d6fcf9d55060a6ae4a4e2dcd34407003ccc853
Ruhan Khandakar <khandakarruhan@gmail.com>
2023-11-09 19:06:44 +0530

    feat: initial run setup

---
+++ pixl/pixl.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"image/color"
+
+	"fyne.io/fyne/v2/app"
+	"ruhan.tech/pixl/apptype"
+	"ruhan.tech/pixl/swatch"
+	"ruhan.tech/pixl/ui"
+)
+
+func main() {
+	pixlApp := app.New()
+	pixlWindow := pixlApp.NewWindow("pixl")
+
+	state := apptype.State{
+		BrushColor:     color.NRGBA{255, 255, 255, 255},
+		SwatchSelected: 0,
+	}
+
+	appInit := ui.AppInit{
+		PixlWindow: pixlWindow,
+		State:      &state,
+		Swatches:   make([]*swatch.Swatch, 0, 64),
+	}
+
+	ui.Setup(&appInit)
+
+	appInit.PixlWindow.ShowAndRun()
+}
--- ui/swatches.go
+++ ui/swatches.go
@@ -12,7 +12,7 @@ func BuildSwatches(app *AppInit) *fyne.Container {
 	canvasSwatches := make([]fyne.CanvasObject, 0, 64)
 
 	for i := 0; i < cap(app.Swatches); i++ {
-		initialColor := color.NRGBA{255, 255, 255, 1}
+		initialColor := color.NRGBA{255, 255, 255, 255}
 
 		s := swatch.NewSwatch(app.State, initialColor, i, func(s *swatch.Swatch) {
 			for j := 0; j < len(app.Swatches); j++ {
