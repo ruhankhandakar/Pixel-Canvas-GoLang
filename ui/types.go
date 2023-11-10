5d02d04594feea8ddeb40ebd28e2e9af51769734
Ruhan Khandakar <khandakarruhan@gmail.com>
2023-11-10 18:14:56 +0530

    render pixel canvas

--- ui/types.go
+++ ui/types.go
@@ -3,10 +3,12 @@ package ui
 import (
 	"fyne.io/fyne/v2"
 	"ruhan.tech/pixl/apptype"
+	"ruhan.tech/pixl/pxcanvas"
 	"ruhan.tech/pixl/swatch"
 )
 
 type AppInit struct {
+	PixlCanvas *pxcanvas.PxCanvas
 	PixlWindow fyne.Window
 	State      *apptype.State
 	Swatches   []*swatch.Swatch
