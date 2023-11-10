c265d5a281e814f2c27805c1df96225c500ac508
Ruhan Khandakar <khandakarruhan@gmail.com>
2023-11-10 18:06:13 +0530

    feat: pixel canvas renderer

---
+++ pxcanvas/pxcanvas.go
@@ -0,0 +1,90 @@
+package pxcanvas
+
+import (
+	"image"
+	"image/color"
+
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/canvas"
+	"fyne.io/fyne/v2/widget"
+	"ruhan.tech/pixl/apptype"
+)
+
+type PxCanvasMouseState struct {
+	previousCoord *fyne.PointEvent
+}
+
+type PxCanvas struct {
+	widget.BaseWidget
+	apptype.PxCanvasConfig
+	renderer    *PxCanvasRenderer
+	PixelData   image.Image
+	mouseState  PxCanvasMouseState
+	appState    *apptype.State
+	reloadImage bool
+}
+
+func (pxCanvas *PxCanvas) Bounds() image.Rectangle {
+	x0 := int(pxCanvas.CanvasOffset.X)
+	y0 := int(pxCanvas.CanvasOffset.Y)
+
+	x1 := int(pxCanvas.PxCols*pxCanvas.PxSize + int(pxCanvas.CanvasOffset.X))
+	y1 := int(pxCanvas.PxRows*pxCanvas.PxSize + int(pxCanvas.CanvasOffset.Y))
+
+	return image.Rect(x0, y0, x1, y1)
+}
+
+func InBounds(pos fyne.Position, bounds image.Rectangle) bool {
+	if pos.X >= float32(bounds.Min.X) &&
+		pos.X < float32(bounds.Max.X) &&
+		pos.Y >= float32(bounds.Min.Y) &&
+		pos.Y < float32(bounds.Max.Y) {
+		return true
+	}
+
+	return false
+}
+
+func NewBlankImage(cols, rows int, c color.Color) image.Image {
+	img := image.NewRGBA(image.Rect(0, 0, cols, rows))
+
+	for y := 0; y < rows; y++ {
+		for x := 0; x < cols; x++ {
+			img.Set(x, y, c)
+		}
+	}
+
+	return img
+}
+
+func (pxCanvas *PxCanvas) CreateRenderer() fyne.WidgetRenderer {
+	canvasImage := canvas.NewImageFromImage(pxCanvas.PixelData)
+	canvasImage.ScaleMode = canvas.ImageScalePixels
+	canvasImage.FillMode = canvas.ImageFillContain
+
+	canvasBorder := make([]canvas.Line, 4)
+
+	for i := 0; i < len(canvasBorder); i++ {
+		canvasBorder[i].StrokeColor = color.NRGBA{100, 100, 100, 255}
+		canvasBorder[i].StrokeWidth = 2
+	}
+
+	renderer := &PxCanvasRenderer{
+		pxCanvas:     pxCanvas,
+		canvasImage:  canvasImage,
+		canvasBorder: canvasBorder,
+	}
+
+	pxCanvas.renderer = renderer
+	return renderer
+}
+
+func NewPxCanvas(state *apptype.State, config apptype.PxCanvasConfig) *PxCanvas {
+	pxCanvas := &PxCanvas{
+		PxCanvasConfig: config,
+		appState:       state,
+	}
+	pxCanvas.PixelData = NewBlankImage(pxCanvas.PxCols, config.PxRows, color.NRGBA{128, 128, 128, 255})
+	pxCanvas.ExtendBaseWidget(pxCanvas)
+	return pxCanvas
+}
