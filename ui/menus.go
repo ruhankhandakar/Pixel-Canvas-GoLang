d22671e484912346e40896b0b121abb2cada07e8
Ruhan Khandakar <khandakarruhan@gmail.com>
2023-11-10 20:03:31 +0530

    feat: load image

--- ui/menus.go
+++ ui/menus.go
@@ -2,6 +2,7 @@ package ui
 
 import (
 	"errors"
+	"image"
 	"image/png"
 	"os"
 	"strconv"
@@ -9,6 +10,7 @@ import (
 	"fyne.io/fyne/v2"
 	"fyne.io/fyne/v2/dialog"
 	"fyne.io/fyne/v2/widget"
+	"ruhan.tech/pixl/util"
 )
 
 func saveFileDialog(app *AppInit) {
@@ -106,10 +108,40 @@ func BuildNewMenu(app *AppInit) *fyne.MenuItem {
 	})
 }
 
+func BuildOpenMenu(app *AppInit) *fyne.MenuItem {
+	return fyne.NewMenuItem("Open...", func() {
+		dialog.ShowFileOpen(func(uri fyne.URIReadCloser, e error) {
+			if uri == nil {
+				return
+			}
+			image, _, err := image.Decode(uri)
+
+			if err != nil {
+				dialog.ShowError(err, app.PixlWindow)
+				return
+			}
+			app.PixlCanvas.LoadImage(image)
+			app.State.SetFilePath(uri.URI().Path())
+
+			imgColors := util.GetImageColors(image)
+
+			i := 0
+			for c := range imgColors {
+				if i == len(app.Swatches) {
+					break
+				}
+				app.Swatches[i].SetColor(c)
+				i++
+			}
+		}, app.PixlWindow)
+	})
+}
+
 func BuildMenus(app *AppInit) *fyne.Menu {
 	return fyne.NewMenu(
 		"File",
 		BuildNewMenu(app),
+		BuildOpenMenu(app),
 		BuildSaveMenu(app),
 		BuildSaveAsMenu(app),
 	)
