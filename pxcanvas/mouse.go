17f08095b901cd38442e263a0bf9d8447bf04eea
Ruhan Khandakar <khandakarruhan@gmail.com>
2023-11-10 19:10:13 +0530

    pixel border display

--- pxcanvas/mouse.go
+++ pxcanvas/mouse.go
@@ -15,6 +15,10 @@ func (pxCanvas *PxCanvas) MouseMoved(ev *desktop.MouseEvent) {
 
 	if x, y := pxCanvas.MouseToCanvasXY(ev); x != nil && y != nil {
 		brush.TryBrush(pxCanvas.appState, pxCanvas, ev)
+		cursor := brush.Cursor(pxCanvas.PxCanvasConfig, pxCanvas.appState.BrushType, ev, *x, *y)
+		pxCanvas.renderer.SetCursor(cursor)
+	} else {
+		pxCanvas.renderer.SetCursor(make([]fyne.CanvasObject, 0))
 	}
 
 	pxCanvas.TryPan(pxCanvas.mouseState.previousCoord, ev)
